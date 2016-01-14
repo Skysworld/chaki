b846aa754f8192c96a88bf45a3151247615c45eb
chaki <chaki.abderrazak@gmail.com>
2016-01-14 15:01:29 +0100

    ajout du php dans l html

--- Addition.php
+++ Addition.php
@@ -1,20 +1,19 @@
 <?php
-//require_once "phpwsdl\class.phpwsdl.php";
-//PhpWsdl::RunQuickMode();
-class addition{
-	public function somme($x,$y){
-		return $x+$y; 	
+	require "index.html";	
+	class addition{
+		public function somme($x,$y){
+			return $x+$y; 	
+		}
 	}
-}
-$options= array( 
-	'location' => 'http://172.17.50.155/chaki/Addition.php', //localhost
-	'uri'=> 'http://172.17.50.155/chaki/' //adresse
-);
+	
+	$options= array( 'location' => 'http://172.17.50.155/chaki/Addition.php', //localhost
+					 'uri'=> 'http://172.17.50.155/chaki/' //adresse
+	);
 
-//service
-$server = new SoapServer(Null,$options); //start server
-//$soap_server -> setFunction('somme'); //assignement de la fonction
-$server->setClass('addition');
-$server->handle();
-echo "<script>alert(\"le programme ne comporte pas d'erreur\")</script>"; 
+	//service
+	$server = new SoapServer(Null,$options); //start server
+	$server->setClass('addition'); //assignement de la class
+	$server->handle();
+	echo "<script>alert(\"le programme ne comporte pas d'erreur\")</script>"; 
+
 ?>
