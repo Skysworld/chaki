e3e11a59c3c83bcbdc030dffa8c6c1ebcd8f9a35
chaki <chaki.abderrazak@gmail.com>
2016-02-04 16:20:46 +0100

    Implimentation des fonctions authentification d'un client et modification des données (complet)

--- client.php
+++ client.php
@@ -8,27 +8,27 @@
 	$options = array('uri'=> 'http://localhost/webservice/');
 	$client = new SoapClient("wsdl.wsdl"); 
 	try {
-		print "\n\n ";	/*
-		$Identifiant ='paul';
-		$Mac='b4:pp:a4:yy';
-		$Broche = array(1,1,0,1,1);
-		echo "Adresse Mac prise : $Mac <br> Broche : ";
-		echo "<br> ";
-		echo $client->etat($Identifiant, $Mac, $Broche); */
+		print "\n\n ";	
+		$Identifiant ='bleu';
+		$Mac='41:45:pm';
+		$Broche = array(1,0,0,1,0);
 
-		echo "<br> <br> <br> ";
-		$Id ='vert';
-		$Password='123';
-		echo "Demande de connexion : ";
-		echo $client->authentification($Id, $Password); 
-		
+		echo "Adresse Mac prise : $Mac <br> Broche : ";
+		print_r($Broche);
+		echo "<br> ";
+		echo $client->modification($Identifiant, $Mac, $Broche); 
+
 		echo "<br> <br> <br> ";
 		$Id ='bleu';
 		$Password='123456';
-		echo "Demande de connexion : ";		
+		echo "Demande de connexion 1 : ";
+		echo $client->authentification($Id, $Password); 
+		
+		echo "<br> <br> <br> ";
+		$Id ='zefd';
+		$Password='787';
+		echo "Demande de connexion 2 : ";		
 		echo $client->authentification($Id, $Password);
-
-
 		
 			
 	} catch (SoapFault $exception) { 
--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * @param $operation Type de l'opération (add/substract)
- * @param $bouboul boolean
- * @return $resultat Résultat de l'opération
+ * @param 
+ * @param 
+ * @return 
  */
  
  
@@ -13,57 +13,57 @@ header("Refresh: 2;"); // permet de faire un refresh
 //require_once("base.php");
 // FIN BASE DE DONNEE
 function authentification($Id,$Password){
-	//if (isset($Id['identifiant']) && isset($Password['motdepasse'])){
-		try{
-			//Connexion à my sql
-			$bdd = new PDO('mysql:host=localhost;dbname=domotique', 'root', '');
-		}	
-		catch (Exception $e){
-			//En cas d'erreur de connexion
-			die('Erreur : '.$e->getMessage());
-		}
-		
-		$sql="SELECT Mac FROM identifiant join prise ON identifiant.Identi=prise.Identity WHERE Identi='$Id' AND Password='$Password'";
-		
-		//$requete = $bdd->query("SELECT Identi FROM identifiant WHERE Identi='$Id' AND Password='$Password'");
-		$requete = $bdd->query($sql);
-		  //  if ($login=="toto" && $password=="titi")
-		if ($requete){
-			 
-			$result=mysql_query($requete);
-			return $requete;
-			//return 'Bienvenu';
-		}
-		else{
-			//la selection dans la base de donnée se fait bien
-		//	echo 
-
-			return 'erreur d identification';
-			//$result=mysql_query($requete);
-
-		}
-	
-
-	//}
-}
-
-function etat($Identifiant,$Mac,$Broche) {
 	try{
 		//Connexion à my sql
-		$bdd = new PDO('mysql:host=localhost;dbname=domotique', 'root', '');
+		mysql_connect("localhost", "root", "");
+		mysql_select_db("domotique");
 	}
 	catch (Exception $e){
 		//En cas d'erreur de connexion
 		die('Erreur : '.$e->getMessage());
 	}
-
-	$requete = $bdd->query("INSERT INTO prise(Identifiant,Mac, EtatBool1, EtatBool2, EtatBool3,EtatBool4, EtatBool5, date) Value('$Identifiant','$Mac','$Broche[0]','$Broche[1]','$Broche[2]','$Broche[3]','$Broche[4]')");
 	
-	echo 'Infos ajoutées';
-	
-	mysql_close();
-	return $Identifiant;
+	try{		
+		$reponse = mysql_query("SELECT Mac FROM identifiant join prise ON identifiant.Identi=prise.Identity WHERE Identi='$Id' AND Password='$Password'");
+		if(mysql_num_rows($reponse)==0)
+		{
+			return 'Pas de connexion';
+		}
+		else{
+			/*while($row = $reponse->fetch_assoc())
+			{
+				echo ("Utilisateur $row[Id] as la prise suivante : $row[Mac] <br>");
+			}*/
+			//return 'connexion reussie';
+			while($row=mysql_fetch_assoc($reponse)){
+				return $row['Mac'];
+			}
+		}
+	}
+	catch (Exception $a){
+		die ('Erreur:'.$a->getMessage());
+	}
+}
 
+function modification($Identifiant,$Mac,$Broche) {
+	try{
+		//Connexion à my sql
+		mysql_connect("localhost", "root", "");
+		mysql_select_db("domotique");
+	}
+	catch (Exception $e){
+		//En cas d'erreur de connexion
+		die('Erreur : '.$e->getMessage());
+	}
+	try{
+	//$requete = $bdd->query("INSERT INTO prise(Identifiant,Mac, EtatBool1, EtatBool2, EtatBool3,EtatBool4, EtatBool5, date) Value('$Identifiant','$Mac','$Broche[0]','$Broche[1]','$Broche[2]','$Broche[3]','$Broche[4]')");
+	$requete= mysql_query("UPDATE prise SET EtatBool1='$Broche[0]', EtatBool2='$Broche[1]', EtatBool3='$Broche[2]', EtatBool4='$Broche[3]', EtatBool5='$Broche[4]' WHERE Mac='$Mac' AND Identity='$Identifiant'");
+	return 'Infos ajoutees';
+	//return $Broche[1];
+	}
+	catch (Exception $a){
+		die ('Erreur:'.$a->getMessage());
+	}
 }
 
 
@@ -76,7 +76,7 @@ ini_set("soap.wsdl_cache_enabled", "0");
 try { 
   $server = new SoapServer('wsdl.wsdl');
   // On ajoute les méthodes que le serveur va gérer
-  $server->addFunction("etat"); 
+  $server->addFunction("modification"); 
   $server->addFunction("authentification"); 
 
 	  
@@ -92,7 +92,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 else {
   echo '<strong>This SOAP server can handle following functions : </strong>';
   echo '<ul>';
-  
   foreach($server->getFunctions() as $func) {
     echo '<li>' , $func , '</li>';
   }
